2e5118a8db12571c5aa8c9f011c5a80d3ffec7ad
shyo21 <shyo21@kaist.ac.kr>
2021-12-28 21:33:09 +0900

    feat : color change . 타이틀바 삭제, 상단바와 버튼 색상 변경 테스트 .

--- app/src/main/java/com/example/proj1/MainActivity.java
+++ app/src/main/java/com/example/proj1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.proj1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "아?루", Toast.LENGTH_LONG).show();
+    }
+
 }
