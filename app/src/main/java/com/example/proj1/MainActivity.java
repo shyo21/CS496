448043b54e922eaa9ab681756586038166e912ce
shyo21 <shyo21@kaist.ac.kr>
2021-12-29 14:30:15 +0900

    feat : tab layout added \n 1. 뷰페이저를 이용한 기초적인 탭 레이아웃 도입  - 스와이프를 이용한 탭간 이동 지원  - 탭 메뉴 터치를 통한 탭간 이동 지원 \n 2. 기본 테마색 skyblue로 설정 \n 3. Fragment 1~3까지 추가  - 현재는 textview 기능만 존재

--- app/src/main/java/com/example/proj1/MainActivity.java
+++ app/src/main/java/com/example/proj1/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.proj1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onButton1Clicked(View v) {
-        Toast.makeText(this, "아?루", Toast.LENGTH_LONG).show();
+        ViewPager vp = findViewById(R.id.viewpager);
+        VPadapter ad = new VPadapter(getSupportFragmentManager());
+        vp.setAdapter(ad);
+
+        TabLayout tab = findViewById(R.id.tab);
+        tab.setupWithViewPager(vp);
     }
 
 }
