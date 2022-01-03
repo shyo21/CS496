c944f250cc9382cbccf6ebd0ac41b6d7871bd410
shyo21 <shyo21@kaist.ac.kr>
2022-01-03 15:55:34 +0900

    feat : Tab 3 prototype - 이건 답이없다 - 테스트용 임시레이아웃 - 아마 싹 갈아엎을듯

--- app/src/main/java/com/example/proj1/Frag3.java
+++ app/src/main/java/com/example/proj1/Frag3.java
@@ -1,64 +1,52 @@
 package com.example.proj1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Frag3#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Frag3 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Frag3() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Frag3.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Frag3 newInstance(String param1, String param2) {
-        Frag3 fragment = new Frag3();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ConstraintLayout btn1;
+    private ImageButton btn2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag3, container, false);
+        View myView = inflater.inflate(R.layout.fragment_frag3, container, false);
+
+        // check display size, calculate optimized column width
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        requireActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int width = displaymetrics.widthPixels;
+        int optWidth = (int) (0.2 * width);
+
+        // Set button 1 and click handler for it
+        btn1 = (ConstraintLayout) myView.findViewById(R.id.imageFrame1);
+        ConstraintLayout.LayoutParams params1 = new ConstraintLayout.LayoutParams(optWidth, (int) (1.15*optWidth));
+        params1.setMargins(100,10,0,0);
+        btn1.setLayoutParams(params1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), btn1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        return myView;
     }
 }
