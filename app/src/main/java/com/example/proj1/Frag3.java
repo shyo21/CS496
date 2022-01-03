4e9d52e9c889738e9d2440c1af2dbeacb18c2ba3
shyo21 <shyo21@kaist.ac.kr>
2022-01-03 16:43:21 +0900

    feat : Tab 3 layout done - 일단 4개의 아이콘 정렬 완료 - 시간이 남으면 2개 더 - 안남으면 4개에 맞춰서 재정렬 필요 - 아이콘 개선 필요 (테두리)

--- app/src/main/java/com/example/proj1/Frag3.java
+++ app/src/main/java/com/example/proj1/Frag3.java
@@ -2,28 +2,16 @@ package com.example.proj1;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class Frag3 extends Fragment {
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 
-    private ConstraintLayout btnFrame1;
-    private ImageView btn1;
-    private ConstraintLayout btnFrame2;
-    private ImageView btn2;
-    private ConstraintLayout btnFrame3;
-    private ImageView btn3;
-    private ConstraintLayout btnFrame4;
-    private ImageView btn4;
+public class Frag3 extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,67 +31,55 @@ public class Frag3 extends Fragment {
         int optMargin = (int) (0.25 * width);
 
         // Set button 1 and click handler for it
-        btnFrame1 = (ConstraintLayout) myView.findViewById(R.id.imageFrame1);
-        btn1 = (ImageView) myView.findViewById(R.id.imageView1);
+        ConstraintLayout btnFrame1 = myView.findViewById(R.id.imageFrame1);
+        ImageView btn1 = myView.findViewById(R.id.imageView1);
         ConstraintLayout.LayoutParams params1 = (ConstraintLayout.LayoutParams) btnFrame1.getLayoutParams();
         params1.setMargins(optMargin,optMargin,0,0);
         params1.width = optWidth;
         params1.height = (int) (1.15*optWidth);
         btnFrame1.setLayoutParams(params1);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), btn1Activity.class);
-                startActivity(intent);
-            }
+        btn1.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), btn1Activity.class);
+            startActivity(intent);
         });
 
         // Set button 2 and click handler for it
-        btnFrame2 = (ConstraintLayout) myView.findViewById(R.id.imageFrame2);
-        btn2 = (ImageView) myView.findViewById(R.id.imageView2);
+        ConstraintLayout btnFrame2 = myView.findViewById(R.id.imageFrame2);
+        ImageView btn2 = myView.findViewById(R.id.imageView2);
         ConstraintLayout.LayoutParams params2 = (ConstraintLayout.LayoutParams) btnFrame2.getLayoutParams();
         params2.setMargins(0,optMargin,optMargin,0);
         params2.width = optWidth;
         params2.height = (int) (1.15*optWidth);
         btnFrame2.setLayoutParams(params2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), btn2Activity.class);
-                startActivity(intent);
-            }
+        btn2.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), btn2Activity.class);
+            startActivity(intent);
         });
 
         // Set button 3 and click handler for it
-        btnFrame3 = (ConstraintLayout) myView.findViewById(R.id.imageFrame3);
-        btn3 = (ImageView) myView.findViewById(R.id.imageView3);
+        ConstraintLayout btnFrame3 = myView.findViewById(R.id.imageFrame3);
+        ImageView btn3 = myView.findViewById(R.id.imageView3);
         ConstraintLayout.LayoutParams params3 = (ConstraintLayout.LayoutParams) btnFrame3.getLayoutParams();
         params3.setMargins(optMargin,100,0,0);
         params3.width = optWidth;
         params3.height = (int) (1.15*optWidth);
         btnFrame3.setLayoutParams(params3);
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), btn3Activity.class);
-                startActivity(intent);
-            }
+        btn3.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), btn3Activity.class);
+            startActivity(intent);
         });
 
         // Set button 3 and click handler for it
-        btnFrame4 = (ConstraintLayout) myView.findViewById(R.id.imageFrame4);
-        btn4 = (ImageView) myView.findViewById(R.id.imageView4);
+        ConstraintLayout btnFrame4 = myView.findViewById(R.id.imageFrame4);
+        ImageView btn4 = myView.findViewById(R.id.imageView4);
         ConstraintLayout.LayoutParams params4 = (ConstraintLayout.LayoutParams) btnFrame4.getLayoutParams();
         params4.setMargins(0,100,optMargin,0);
         params4.width = optWidth;
         params4.height = (int) (1.15*optWidth);
         btnFrame4.setLayoutParams(params4);
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), btn4Activity.class);
-                startActivity(intent);
-            }
+        btn4.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), btn4Activity.class);
+            startActivity(intent);
         });
 
         return myView;
